ebaa0f4d3be8dce79feab24c8a218be5abc7bbda
Brian Otto <brian.otto@zoho.com>
2024-10-07 23:30:38 -0700

    Added the correct version

--- src/include/version.h
+++
@@ -1,9 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-
-#define PACKAGE_NAME "Deus Magi Client"
-#define PACKAGE_VERSION_MAJOR 4
-#define PACKAGE_VERSION_MINOR 0
-#define PACKAGE_VERSION_PATCH 0
-
-#endif
