a224bd042de6341b4efd5e6f5d39d5fc4b60ca81
Brian Otto <brian.otto@zoho.com>
2024-11-04 21:45:09 -0800

    Added WSL error detection to the batch script

--- build/DeusMagi.bat
+++ build/DeusMagi.bat
@@ -1,3 +1,12 @@
-REM Add WSL2 detection and install
+@ECHO OFF
 
 START "Deus Magi" wsl ./DeusMagi.AppImage &
+
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO:
+    ECHO You must install WSL v2 and all dependencies before running Deus Magi ...
+    ECHO:
+    ECHO wsl --install
+    ECHO:
+    ECHO Please see the Client Requirements in the README for a list of the dependencies.
+)
